c071d8408d650c54411454ca9eba3336cc56b832
fitzpaky <kfitzpatrick44@gmail.com>
2020-06-07 11:34:24 -0700

    adding search and saved

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,11 +2,22 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+    <>
+    <nav>
+      <div className="nav-wrapper">
+      <a href="#" class="brand-logo">Google Books Search</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+          <li><a href="">Search</a></li>
+          <li><a href="">Saved</a></li>
+        </ul>
+      </div>
     </nav>
+    <ul class="sidenav" id="mobile-demo">
+    <li><a href="">Search</a></li>
+    <li><a href="">Saved</a></li>
+  </ul>
+  </>
   );
 }
 
